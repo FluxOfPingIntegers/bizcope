891531f3d0615d52a9bf9fa6363c7def1a5354da
Ryan Schleck <ryan.m.schleck@gmail.com>
2023-05-25 14:42:58 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Bizcope
+
+## Description
+The Bizcope API is a Node/Express application. It is designed to scrape LinkedIn accounts and email addresses from any given URL and present the results in JSON format.
+
+The API utilizes the following technologies:
+- Node.js
+- Express.js
+- Axios
+- Cheerio
+- URL
+
+## Getting Started
+To get started with the Bizcope API, follow the installation instructions below.
+
+## Installation
+1. Clone the repository to your local machine.
+2. Navigate to the project directory.
+3. Install the dependencies by running the following command:
+```bash
+npm install
+```
+4. Start the API server with the following command:
+```bash
+node app.js
+```
+5. The server will start listening on port 3000. You can access the API endpoints using `http://localhost:3000`.
+
+## Usage
+To use the Bizcope API, make a GET request to the `/scrape/*` endpoint with the desired URL to scrape. The URL should be URL-encoded and appended to the `/scrape/` path.
+
+Example:
+```bash
+GET /scrape/http%3A%2F%2Fexample.com
+```
+
+The API will scrape the provided URL for LinkedIn accounts and email addresses. The results will be returned in JSON format, containing two arrays: `emails` and `linkedInAccounts`. The `emails` array will contain the scraped email addresses, while the `linkedInAccounts` array will contain the scraped LinkedIn account URLs.
+
+## Contributing
+This is a personal project and I am not looking for Contributions at this time.
+
+## Author
+The Bizcope API was created by Ryan Schleck - [LinkedIn](https://www.linkedin.com/in/ryan-schleck/) | [Blog](https://ryan-m-schleck.medium.com/)
+
+## License
+This project is licensed under the MIT License. See the [MIT](https://choosealicense.com/licenses/mit/) file for details.
+
---
+++ app.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const url = require('url');
+const app = express();
+
+app.get('/scrape/*', (req, res) => {
+  const domain = req.params[0];
+  const baseUrl = decodeURIComponent(domain);
+
+  const emails = new Set();
+  const linkedInAccounts = new Set();
+  const visitedUrls = new Set();
+  const urlsToVisit = [baseUrl];
+
+  function crawl() {
+    if (urlsToVisit.length === 0) {
+      res.json({ emails: [...emails], linkedInAccounts: [...linkedInAccounts] });
+      return;
+    }
+
+    const nextUrl = urlsToVisit.pop();
+    visitedUrls.add(nextUrl);
+
+    axios.get(nextUrl)
+      .then(response => {
+        const html = response.data;
+
+        if (response.status === 200) {
+          const $ = cheerio.load(html);
+
+          $('a').each((i, link) => {
+            const href = $(link).attr('href');
+
+            if (href) {
+              if (href.startsWith('mailto:')) {
+                const email = href.replace('mailto:', '');
+                emails.add(email);
+              } else if (href.includes('linkedin.com')) {
+                const linkedInUrl = new URL(href);
+                const linkedInAccount = "https://www.linkedin.com/" + linkedInUrl.pathname.substr(1);
+                linkedInAccounts.add(linkedInAccount);
+              } else {
+                const absoluteUrl = url.resolve(baseUrl, href);
+                if (absoluteUrl.startsWith(baseUrl) && !visitedUrls.has(absoluteUrl)) {
+                  urlsToVisit.push(absoluteUrl);
+                }
+              }
+            }
+          });
+
+          const emailRegex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g;
+          const text = $('body').text();
+          const matches = text.match(emailRegex);
+
+          if (matches) {
+            for (const match of matches) {
+              if (match.endsWith(`@${domain}`)) {
+                emails.add(match);
+              }
+            }
+          }
+        }
+
+        crawl();
+      })
+      .catch(error => {
+        console.log('Error:', error.message);
+        crawl();
+      });
+  }
+
+  if (baseUrl.startsWith('https://')) {
+    axios.head(baseUrl)
+      .then(response => {
+        if (response.status === 200) {
+          crawl();
+        } else {
+          const secureBaseUrl = `https://${domain}`;
+          urlsToVisit[0] = secureBaseUrl;
+          crawl();
+        }
+      })
+      .catch(error => {
+        console.log('Error:', error.message);
+        crawl();
+      });
+  } else {
+    crawl();
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
